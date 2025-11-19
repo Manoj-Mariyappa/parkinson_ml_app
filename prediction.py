d1b4d3b81f9299f79fd914043cb61c9f2bc9bbd6
Manoj-Mariyappa <200231manoj@gmail.com>
2025-11-19 11:02:15 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -50,7 +50,12 @@ if selected == 'Clinical Data Test':
         Fhi = st.text_input('MDVP:Fhi(Hz)')
         NHR = st.text_input('NHR')
         APQ5 = st.text_input('Shimmer:APQ5')
+        
+    clear_btn = st.form_submit_button("🧹 Clear")
 
+    if clear_btn:
+        st.experimental_rerun()
+    
     if st.button("\U0001F50D Check Result"):
         if parkinson_model and scaler:
             try:
@@ -184,6 +189,7 @@ if selected == "Chat Helper":
     if st.button("🗑️ Clear Chat"):
         st.session_state.chat_history = [{"role": "system", "content": "You are a helpful medical assistant. Only answer questions related to Parkinson’s disease."}]
         st.rerun()
+
 
 
 
